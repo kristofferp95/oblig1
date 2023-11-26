d1469edfbc29f322559a2c4bfd75e9aa1df968ae
Kristoffer Storhaug Pedersen <kristoffer.s.pedersen1@gmail.com>
2023-11-26 09:29:54 +0100

    updates

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import os
-import tempfile
-import zipfile
-import io
-import PyPDF2
-import matplotlib
+import os, tempfile, zipfile, io, matplotlib, base64, docx, cv2, numpy as pd
 matplotlib.use('Agg')
-import base64
-import cv2
-import numpy as np
 from flask import Flask, request, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
 import matplotlib.pyplot as plt
@@ -18,145 +10,163 @@ from sklearn.decomposition import PCA
 
 app = Flask(__name__)
 
-# Konfigurasjoner
-app.config['UPLOAD_FOLDER'] = 'uploads/'
-app.config['ALLOWED_EXTENSIONS'] = {'zip'}
+# Configuration settings
+app.config['UPLOAD_FOLDER'] = 'uploads/'  # Directory for storing uploaded files
+app.config['ALLOWED_EXTENSIONS'] = {'zip'}  # Allowed file extensions for upload
+
+def allowed_file(filename):
+    # Check if the file has a valid extension
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/', methods=['GET', 'POST'])
 def upload():
+    # Handles file uploads
     if request.method == 'POST':
-        # Sjekk om POST-anmodningen har filen delen
+        # Check if the post request has the file part
         if 'file' not in request.files:
             return redirect(request.url)
         file = request.files['file']
+        # If no file is selected, redirect
         if file.filename == '':
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-
+            file.save(filepath)  # Save the file
             return redirect(url_for('result', filename=filename))
-    return render_template('upload.html')
+    return render_template('upload.html')  # Render the upload form
 
 @app.route('/result/<filename>')
 def result(filename):
+    # Displays the results after processing the uploaded file
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
     with zipfile.ZipFile(filepath, 'r') as zip_ref:
         file_list = zip_ref.namelist()
+        # Process images if found
         if any(file.lower().endswith(('.png', '.jpg', '.jpeg')) for file in file_list):
             fig = process_zip_file(filepath)
-        elif any(file.lower().endswith(('.txt', '.pdf')) for file in file_list):
+        # Process text files if found
+        if any(file.lower().endswith(('.txt', '.pdf', '.docx')) for file in file_list):
             fig = process_text_zip_file(filepath)
-        else:
-            return "Unsupported file type in ZIP", 400
 
     img = io.BytesIO()
     fig.savefig(img, format='png', bbox_inches='tight')
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode()
-
-    return render_template('result.html', image_plot=plot_url)
-
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'zip', 'mp3', 'wav'}
+    return render_template('result.html', image_plot=plot_url)  # Display results
 
 def process_zip_file(filepath, n_clusters=3, size=(64, 64)):
+    # Process a zip file containing images
     with tempfile.TemporaryDirectory() as temp_dir:
+        # Extract the zip file contents
         with zipfile.ZipFile(filepath, 'r') as zip_ref:
             zip_ref.extractall(temp_dir)
 
+        # Get the base folder name from the zip file
         folder_name = os.path.splitext(os.path.basename(filepath))[0]
         folder_path = os.path.join(temp_dir, folder_name)
 
+        # List all image files in the extracted folder
         image_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
 
+        # Check if the number of images is sufficient for clustering
         if len(image_files) < n_clusters:
-            raise ValueError(f"Antall bilder ({len(image_files)}) er mindre enn antall klynger ({n_clusters}).")
+            raise ValueError(f"Number of images ({len(image_files)}) is less than the number of clusters ({n_clusters}).")
 
+        # Process each image and flatten it
         image_data = [process_image_file(file, size) for file in image_files]
 
+        # Normalize the image data
         scaler = StandardScaler()
         images_normalized = scaler.fit_transform(image_data)
 
+        # Reduce dimensions using PCA
         pca = PCA(n_components=2)
         principal_components = pca.fit_transform(images_normalized)
 
+        # Perform K-means clustering
         kmeans = KMeans(n_clusters=n_clusters, n_init=10)
         clusters = kmeans.fit_predict(principal_components)
 
+        # Create a scatter plot of the clustered images
         fig, ax = plt.subplots()
         scatter = ax.scatter(principal_components[:, 0], principal_components[:, 1], c=clusters, cmap='viridis')
-        ax.set_title('Bilde Clustering')
+        ax.set_title('Image Clustering')
+        plt.xlabel('Principal Component 1')
+        plt.ylabel('Principal Component 2')
         plt.colorbar(scatter, ax=ax)
-        return fig  # Returnerer matplotlib-figuren
+        return fig  # Return the matplotlib figure
 
 def process_image_file(filepath, size):
-    img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-    img_resized = cv2.resize(img, size)
-    img_flattened = img_resized.flatten()
-    return img_flattened
-
-def extract_text_from_pdf(file_path):
-    with open(file_path, 'rb') as file:
-        reader = PyPDF2.PdfFileReader(file)
-        text = ""
-        for page in range(reader.numPages):
-            text += reader.getPage(page).extractText()
-        return text
+    # Process an individual image file for analysis
+    img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)  # Read the image in grayscale
+    img_resized = cv2.resize(img, size)  # Resize the image
+    img_flattened = img_resized.flatten()  # Flatten the image data
+    return img_flattened  # Return the flattened image data
 
 def extract_text_from_txt(file_path):
+    # Extract text from a .txt file
     with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
+        return file.read()  # Return the file content
+
+def extract_text_from_docx(file_path):
+    # Extract text from a .docx file
+    doc = docx.Document(file_path)
+    text = [paragraph.text for paragraph in doc.paragraphs]  # Extract text from each paragraph
+    return '\n'.join(text)  # Join and return the text
 
 def process_text_zip_file(filepath, n_clusters=3, segment_length=100):
+    # Process a zip file containing text documents
     with tempfile.TemporaryDirectory() as temp_dir:
+        # Extract the zip file contents
         with zipfile.ZipFile(filepath, 'r') as zip_ref:
             zip_ref.extractall(temp_dir)
 
         text_segments = []
+        # Traverse through the extracted files and extract text
         for root, dirs, files in os.walk(temp_dir):
             for filename in files:
-                if filename.lower().endswith('.txt'):
-                    text = extract_text_from_txt(os.path.join(root, filename))
-                elif filename.lower().endswith('.pdf'):
-                    text = extract_text_from_pdf(os.path.join(root, filename))
-                else:
-                    continue
-                text_segments.extend([text[i:i+segment_length] for i in range(0, len(text), segment_length)])
+                # Ignore hidden and system files
+                if not filename.startswith('._') and not filename.startswith('.__'):
+                    file_path = os.path.join(root, filename)
+                    # Process text files based on their extensions
+                    if filename.lower().endswith('.txt'):
+                        text = extract_text_from_txt(file_path)
+                    elif filename.lower().endswith('.docx'):
+                        text = extract_text_from_docx(file_path)
+                    else:
+                        continue
+                    # Break the text into segments for processing
+                    text_segments.extend([text[i:i+segment_length] for i in range(0, len(text), segment_length)])
 
         if not text_segments:
-            raise ValueError("Ingen tekst funnet i filene.")
+            raise ValueError("No text found in files.")
 
-        # Vektoriser tekstsegmentene
+        # Vectorize the text segments
         vectorizer = CountVectorizer(analyzer='char')
         X = vectorizer.fit_transform(text_segments).toarray()
 
-        # Normaliser vektorene
+        # Normalize the vectors
         scaler = StandardScaler()
         text_vectors_normalized = scaler.fit_transform(X)
 
-        # K-means clustering
+        # Apply K-means clustering
         kmeans = KMeans(n_clusters=n_clusters, random_state=0)
         clusters = kmeans.fit_predict(text_vectors_normalized)
 
-        # Bruk PCA for dimensjonsreduksjon for visualisering
+        # Reduce dimensions using PCA for visualization
         pca = PCA(n_components=2)
         principal_components = pca.fit_transform(text_vectors_normalized)
 
-        # Visualisering
+        # Visualize the clustering of text segments
         fig, ax = plt.subplots(figsize=(10, 8))
         scatter = ax.scatter(principal_components[:, 0], principal_components[:, 1], c=clusters, cmap='viridis')
-        ax.set_title('Visualisering av Tekst Clustering med PCA')
+        ax.set_title('Text Clustering Visualization with PCA')
         plt.colorbar(scatter, ax=ax)
         plt.xlabel('Principal Component 1')
         plt.ylabel('Principal Component 2')
         plt.close(fig)
-        return fig  # Returnerer matplotlib-figuren
-
+        return fig  # Return the matplotlib figure for display
 
 
 if __name__ == '__main__':
